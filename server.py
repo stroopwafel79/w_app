b265fec5fe63f3ea647a235513c47c14f1d02206
stroopwafel79 <stroopwafel79@gmail.com>
2019-03-20 22:16:10 +0000

    Added skeleton of server.py and index.html

---
+++ server.py
@@ -0,0 +1,36 @@
+"""Flask server to run my app on"""
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, url_for, jsonify)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "12345"
+
+# Normally, if you use an indefined variable in Jinja2, it fails
+# silently. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def show_homepage():
+    """ Show the homepage"""
+
+    return render_template("homepage.html")
+
+
+if __name__ == '__main__':
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    # Use the DebugToolbar
+    #DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
